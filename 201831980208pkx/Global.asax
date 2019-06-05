3335e64ac2931e003f6cf096d911dc6837b35fca
DELL <DELL@DESKTOP-2857DVS>
2019-06-05 19:17:28 +0800

    MyAction

---
+++ 201831980208pkx/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_201831980208pkx.MvcApplication" Language="C#" %>
---
+++ 201831980208pkx/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_201831980208pkx.Startup))]
+namespace _201831980208pkx
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
